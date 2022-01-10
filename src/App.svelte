0196aae813a1fa543030219ac394595444cb493c
bmehder <bmehder@gmail.com>
2022-01-10 11:14:39 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { user } from './sessionStore'
+  import { supabase } from './supabaseClient'
+  import Auth from './Auth.svelte'
+  import Profile from './Profile.svelte'
+
+  user.set(supabase.auth.user())
+
+  supabase.auth.onAuthStateChange((_, session) => {
+    user.set(session.user)
+  })
+</script>
+
+<main>
+  <div>
+    {#if $user}
+      <Profile />
+    {:else}
+      <Auth />
+    {/if}
+  </div>
+</main>
+
+<style>
+  main {
+    max-width: 601px;
+    margin: 2rem auto;
+  }
+  div {
+    padding: 50px 0 100px 0;
+  }
+  @media screen and (max-width: 600px) {
+    main {
+      max-width: 80%;
+    }
+  }
+</style>
